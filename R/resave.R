e327b3bd536333e8556589be8b98734536c77d07
superuser <alfonso.r.reyes@gmail.com>
2017-07-25 11:36:11 -0500

    add tests for m.spdiags

---
+++ R/resave.R
@@ -0,0 +1,11 @@
+#' Appends, saves variables to RDA file
+#' @param ... additional parameters
+#' @param list a list
+#' @param file a RDA file
+#' @export
+resave <- function(..., list = character(), file) {
+    previous  <- load(file)
+    var.names <- c(list, as.character(substitute(list(...)))[-1L])
+    for (var in var.names) assign(var, get(var, envir = parent.frame()))
+    save(list = unique(c(previous, var.names)), file = file)
+}
