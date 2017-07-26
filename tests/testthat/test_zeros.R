4ebb25bc6e47923cd034413d8a384ef3ae300826
superuser <alfonso.r.reyes@gmail.com>
2017-07-26 18:28:38 -0500

    add notebooks to test MRST

---
+++ tests/testthat/test_zeros.R
@@ -0,0 +1,23 @@
+library(testthat)
+
+context("test zeros")
+
+# q = zeros(N, 1);
+
+test_that("zeros match 1D", {
+    N <- 5
+    expected <- array(0, N)
+    expect_equal(zeros(N), expected)
+})
+
+test_that("zeros match 2D", {
+    N <- 5
+    expected <- array(0, c(N, 10))
+    expect_equal(zeros(N, 10), expected)
+})
+
+test_that("zeros match 3D", {
+    N <- 5
+    expected <- array(0, c(N, 10, 7))
+    expect_equal(zeros(N, 10, 7), expected)
+})
