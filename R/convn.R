d9f2874efdcc28d3caf4c9f47f78d976ef375c6c
superuser <alfonso.r.reyes@gmail.com>
2017-07-30 08:30:39 -0500

    convolution 3D only works when both arrays are equal

--- R/convn.R
+++ R/convn.R
@@ -19,18 +19,19 @@ m.conv3d <- function(A, B) {
         B <- array(B, dim = c(dim(B), 1))
     }
 
-    if (any(dim(A) < dim(B))) {
-        # cat("array B bigger than A")
-        temp <- A
-        A <- B
-        B <- temp
-    }
+    # if (any(dim(A) < dim(B))) {
+    #     # if array B bigger than A
+    #     temp <- A
+    #     A <- B
+    #     B <- temp
+    # }
 
     D <- array(0, dim = c(2*dim(A)[1]-1, 2*dim(A)[2]-1, 2*dim(A)[3]-1))
 
-    for (i in 1:dim(A)[3]) {  # 1, 2, 3
+        for (i in 1:dim(A)[3]) {  # 1, 2, 3
         D[,,i] <- i *.O$convn(A[,,i], B[,,i])
     }
+
 
     if (dim(A)[3] > 1) {
         for (j in (dim(A)[3]-1):1) {   # 2, 1
